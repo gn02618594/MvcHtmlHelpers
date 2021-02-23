7e910b91a20a469b7a2d36a4833aa5773fc9bb73
gn02618594 <gn02618594@gmail.com>
2021-02-23 23:06:47 +0800

    加入專案檔案。

---
+++ MvcHtmlHelpers/Controllers/HtmlHelperController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcHtmlHelpers.Models;
+
+namespace MvcHtmlHelpers.Controllers
+{
+    public class HtmlHelperController : Controller
+    {
+        // GET: HtmlHelper
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SampleHelpers() {
+            User register = new User
+            {
+                Id = 1001,
+                Name = "楊XX",
+                Nickname = "可以的",
+                Email = "gnxxxxxx@gmail.com",
+                City = 2,
+                Terms = true
+            };
+
+            ViewData.Model = register;
+
+            return View();
+        }
+        public ActionResult ValidationMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ValidationMessage(User user)
+        {
+            if (ModelState.IsValid) {
+                return Content("成功!");
+            }
+            return View(user);
+        }
+    }
+}
