7b7acc1f29996bf685d3c216d57c39124b3838fe
gn02618594 <gn02618594@gmail.com>
2021-03-01 22:01:44 +0800

    feat: 利用 Data.Annotations 為 Model 加上驗證規則

--- MvcHtmlHelpers/Models/Employee.cs
+++ MvcHtmlHelpers/Models/Employee.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcHtmlHelpers.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(20,MinimumLength =3,ErrorMessage ="最少需要3個字元!")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{4}\-?\d{3}\-?\d{3{$",ErrorMessage ="需為 09xx-xxx-xxx")]
         public string Mobile { get; set; }
+        [Required(ErrorMessage ="請輸入 Eamil")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required(ErrorMessage ="請輸入 Department")]
         public string Department { get; set; }
+        [Required(ErrorMessage ="請輸入 Title")]
         public string Title { get; set; }
     }
 }
